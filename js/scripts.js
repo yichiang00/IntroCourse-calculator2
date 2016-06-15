40b8e6ecb2030a3d303a4ecc5cf7c575bf74c629
Andrew Niekamp, Quinton Williams and Shokouh Farvid <andrew+quinton+shokouh@>
2016-06-15 15:04:28 -0700

    add submit and preventdefault to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,14 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-
-// alert(add(number1, number2));
-// alert(subtract(number1, number2));
-// alert(multiply(number1, number2));
-// alert(divide(number1,number2));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+    // alert(subtract(number1, number2));
+    // alert(multiply(number1, number2));
+    // alert(divide(number1,number2));
+  });
+});
