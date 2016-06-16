72c223d534f6426bbc2b52021a07d320e842dfce
Andrew Niekamp and Yi Chiang <andrew+yi@>
2016-06-16 15:35:28 -0700

    Add operator and validation components

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,28 @@ var divide = function(number1, number2) {
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2)
-    $("#output").text(result);
+    var choice = $("input:radio[name=operator]:checked").val();
+    var number1 = parseFloat($("#add1").val());
+    var number2 = parseFloat($("#add2").val());
+    var result;
+
+    if (!number1 || !number2) {
+
+      $("#numberText1").removeClass("has-success").addClass("has-error");
+      $("#numberText2").removeClass("has-success").addClass("has-error");
+      $("#output").html("<h1 id=\"scold\">Please use a number!</h1>");
+    }
+    if (choice === "add") {
+      result = add(number1, number2);
+    } else if (choice === "subtract") {
+      result = subtract(number1, number2);
+    }else if (choice === "multiply") {
+      result = multiply(number1, number2);
+    }else{
+      result = divide(number1, number2);
+    }
+
+    // $("#output").text(result);
     // alert(subtract(number1, number2));
     // alert(multiply(number1, number2));
     // alert(divide(number1,number2));
